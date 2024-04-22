8449e71b7fd794564fa1d7e469e506a89b4d903d
Radu Brehar <radu@jslog.com>
2024-04-22 14:47:50 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Comparing AG Grid to Infinite Table React DataGrid
+
+This is a comparison between AG Grid and Infinite Table React DataGrid. Both are popular DataGrid libraries.
+
+Click thumbnail below to watch comparison video:
+
+<div align="center">
+
+[![AG Grid vs Infinite Table](https://img.youtube.com/vi/3FZG6Eia27Y/0.jpg)](https://www.youtube.com/watch?v=3FZG6Eia27Y)
+
+</div>
+
+## Links
+
+For the **Infinite Table** example repo, see https://github.com/radubrehar/test-infinite-table
+
+NPM links:
+
+- [AG Grid - npm package](https://www.npmjs.com/package/ag-grid-enterprise)
+- [Infinite Table - npm package](https://www.npmjs.com/package/@infinite-table/infinite-react)
+
+Website links:
+- [AG Grid - website](https://www.ag-grid.com/)
+- [Infinite Table React DataGrid - website](https://infinite-table.com/)
+
+This app deployed:
+- [AG Grid deployed example app - this repo](https://perf-aggrid-react.netlify.app/)
+- [Infinite Table deployed example app - this repo](https://perf-infinite-table.netlify.app/)
---
+++ src/App.jsx
@@ -0,0 +1,113 @@
+import * as React from "react";
+import { useState } from "react";
+import { DataGrid } from "@mui/x-data-grid";
+
+import { StyledEngineProvider } from "@mui/material/styles";
+
+const genColumns = [
+  ...Array.from({ length: 100 }, (_, i) => ({
+    field: `col-${i}`,
+  })),
+];
+
+const columns = [
+  { field: "id" },
+  { field: "make" },
+  { field: "model" },
+  { field: "price" },
+
+  // generate 100 more columns
+  ...genColumns,
+];
+
+function addColPropsToObject(obj) {
+  return genColumns.reduce((acc, col, i) => {
+    acc[`col-${i}`] = `${col.field} - ${obj.id}`;
+    return acc;
+  }, obj);
+}
+
+const rowData = [
+  addColPropsToObject({
+    make: "Tesla",
+    model: "Model Y",
+    price: 64950,
+    electric: true,
+    id: "x",
+  }),
+  addColPropsToObject({
+    make: "Ford",
+    model: "F-Series",
+    price: 33850,
+    electric: false,
+    id: "y",
+  }),
+  addColPropsToObject({
+    make: "Toyota",
+    model: "Corolla",
+    price: 29600,
+    electric: false,
+    id: "z",
+  }),
+  // generate another 1000 rows
+  ...Array.from({ length: 1000 }, (_, i) =>
+    addColPropsToObject({
+      make: `Make ${i}`,
+      model: `Model ${i}`,
+      id: `id-${i}`,
+      price: Math.floor(Math.random() * 100000),
+      electric: Math.random() > 0.5,
+    })
+  ),
+];
+
+const getRowId = (data) => data.id;
+
+function getColumns() {
+  const cols = [...columns];
+  const count = localStorage.getItem("columnCount") * 1 || 4;
+  cols.length = count;
+  return cols;
+}
+
+const GridExample = () => {
+  const [colDefs, setColDefs] = useState(getColumns);
+
+  const setColumnCount = (value) => {
+    localStorage.setItem("columnCount", value);
+    setColDefs(getColumns());
+  };
+
+  return (
+    <StyledEngineProvider injectFirst>
+      <div>
+        Grid height: <b>500px</b>. Row height: <b>18px</b>. Row count{" "}
+        <b>{rowData.length}</b>. Column count{" "}
+        <select
+          value={localStorage.getItem("columnCount") || 4}
+          onChange={(event) => {
+            setColumnCount(event.target.value);
+          }}
+        >
+          <option value="4">4</option>
+          <option value="10">10</option>
+          <option value="20">20</option>
+          <option value="50">50</option>
+          <option value="100">100</option>
+        </select>
+        <div
+          style={{ height: "500px", width: "100vw", background: "white" }} // the grid will fill the size of the parent container
+        >
+          <DataGrid
+            getRowId={getRowId}
+            rows={rowData}
+            columns={colDefs}
+            rowHeight={18}
+          />
+        </div>
+      </div>
+    </StyledEngineProvider>
+  );
+};
+
+export default GridExample;
